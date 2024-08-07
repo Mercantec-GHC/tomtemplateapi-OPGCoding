cb7784011d11bd42cea4d9f55d5a696d7c64a08c
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-08-07 14:55:22 +0200

    GRAFEN ER LAVET

--- BlazorApp1/BlazorApp1/wwwroot/Chart.js
+++ BlazorApp1/BlazorApp1/wwwroot/Chart.js
@@ -1,13 +1,21 @@
-﻿//window.renderChart = function (data) {
-//    var ctx = document.getElementById('myChart').getContext('2d');
-//    new Chart(ctx, {
-//        type: 'line',
-//        data: {
-//            labels: data.dates,
-//            datasets: [{
-//                label: 'Price',
-//                data: data.prices
-//            }]
-//        }
-//    });
-//}
+﻿const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+
+new Chart("myChart", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(0,0,255,1.0)",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            yAxes: [{ ticks: { min: 6, max: 16 } }],
+        }
+    }
+});
