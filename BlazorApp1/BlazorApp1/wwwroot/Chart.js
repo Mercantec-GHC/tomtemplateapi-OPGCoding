bb3ae61d07bea2f22883201cf5a3507db0a2ff55
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-08-07 13:32:05 +0200

    Implemented blazor
    Fået data hentet, prøver at lave en graf

---
+++ BlazorApp1/BlazorApp1/wwwroot/Chart.js
@@ -0,0 +1,13 @@
+﻿//window.renderChart = function (data) {
+//    var ctx = document.getElementById('myChart').getContext('2d');
+//    new Chart(ctx, {
+//        type: 'line',
+//        data: {
+//            labels: data.dates,
+//            datasets: [{
+//                label: 'Price',
+//                data: data.prices
+//            }]
+//        }
+//    });
+//}
