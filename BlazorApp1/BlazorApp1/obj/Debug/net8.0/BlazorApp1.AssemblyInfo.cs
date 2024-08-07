a5b46d565d1a7d1ce4a9a5d007565cf372707284
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-08-07 15:03:44 +0200

    Grafen og tabellen ser skide godt ud nu

--- BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3ae61d07bea2f22883201cf5a3507db0a2ff55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7784011d11bd42cea4d9f55d5a696d7c64a08c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
